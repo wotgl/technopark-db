c3c8cfe8e08b2821ffb1f3e0e8d65185df7a6d62
sasha1003 <sasha.konyshev1003@gmail.com>
2015-10-14 19:43:30 +0300

    Create first handler; Create InputRequest;

--- main.go
+++ main.go
@@ -3,11 +3,41 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+type InputRequest struct {
+	method string
+	url    string
+	path   string
+	json   string
+	query  map[string][]string
+}
+
+func (ir *InputRequest) parse(r *http.Request) {
+	ir.method = r.Method
+	ir.url = fmt.Sprintf("%v", r.URL)
+	ir.path = r.URL.EscapedPath()
+
+	// ReadAll reads from r until an error or EOF and returns the data it read
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	ir.json = string(body)
+
+	ir.query = r.URL.Query()
+}
+
+func api(w http.ResponseWriter, r *http.Request) {
+	inputRequest := new(InputRequest)
+	inputRequest.parse(r)
+
+	fmt.Println(inputRequest)
+
 	io.WriteString(w, "Hello world!")
+	fmt.Println("")
 }
 
 func main() {
@@ -15,7 +45,8 @@ func main() {
 
 	fmt.Printf("The server is running on http://localhost%s\n", PORT)
 
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/db/api/", api)
+
 	http.ListenAndServe(PORT, nil)
 
 }
